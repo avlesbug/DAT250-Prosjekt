91a3db4d4e8c3a56f1545458e571b6e208763d6a
Kristian Avlesbug <kristian.avlebsug@student.uib.no>
2021-10-06 12:43:04 +0200

    Fjernet kommentert ut kode

--- dat250-jpa/eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Poll/PollMain.java
+++ dat250-jpa/eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Poll/PollMain.java
@@ -90,47 +90,6 @@ public class PollMain {
         Long voteId2 = vote2.getId();
 
 
-
-        // No, so lets create new entries
-
-        /**
-        if (createNewEntries) {
-            assertTrue(q.getResultList().size() == 0);
-
-            PollDAO pollDAO = new PollDAO();
-            pollDAO.setEmf(factory);
-            PollUserDAO pollUserDAO = new PollUserDAO();
-            pollUserDAO.setEmf(factory);
-
-
-            pollUserDAO.persistPollUser(pollUser);
-
-            List<Poll> pollList = new ArrayList<>();
-            //Poll poll2 = new Poll("Poll name", "Poll question #1", pollUser);
-            //Poll poll2 = new Poll("Second poll", "Do you like polls?", pollUser);
-            pollList.add(poll);
-            pollUser.setPollList(pollList);
-
-
-
-
-            List<Vote> newVotes = new ArrayList<>();
-            for(int i=0; i<20;i++){
-                Vote newVote;
-                if(i%2==1){
-                    newVote = new Vote(Answer.YES,poll);
-                } else {
-                    newVote = new Vote(Answer.NO,poll);
-                }
-                newVotes.add(newVote);
-            }
-            poll.setVotes(newVotes);
-
-            pollDAO.persistPoll(poll);
-        }
-         **/
-
-
         if (args.length > 0) {
             port(Integer.parseInt(args[0]));
         } else {
@@ -382,57 +341,3 @@ public class PollMain {
     }
 
 }
-
-
-/**
- * Gammel Main
- * if (createNewEntries) {
- *             assertTrue(q.getResultList().size() == 0);
- *
- *             //Create user
- *             PollUser pollUser = new PollUser();
- *             PollUser pollUser2 = new PollUser();
- *             pollUser.setName("Max Musterman");
- *             pollUser2.setName("Ola Normann");
- *             pollUser2.setEmail("ola.normann@gmail.com");
- *             pollUser.setEmail("m.musterman@gmail.com");
- *             pollUser.setPassword("Password123");
- *             pollUser2.setPassword("Spania2009");
- *
- *             //Create poll & votes
- *
- *             Poll poll = new Poll();
- *             Poll poll2 = new Poll();
- *             List<Poll> pollList = new ArrayList<>();
- *             poll.setName("Simple poll");
- *             poll.setQuestion("Is this a question?");
- *             poll.setUser(pollUser);
- *             poll2.setName("Second poll");
- *             poll2.setQuestion("Do you like taco?");
- *             poll2.setUser(pollUser);
- *             pollList.add(poll);
- *             pollList.add(poll2);
- *             pollUser.setPollList(pollList);
- *
- *
- *
- *             List<Vote> votes = new ArrayList<>();
- *             Vote vote = new Vote();
- *             vote.setAnswer(1);
- *             Vote vote2 = new Vote();
- *             vote2.setAnswer(0);
- *             vote.setPoll(poll);
- *             vote2.setPoll(poll);
- *             votes.add(vote);
- *             votes.add(vote2);
- *             poll.setVotes(votes);
- *
- *             em.persist(pollUser);
- *             em.persist(poll);
- *             em.persist(poll2);
- *             em.persist(vote);
- *             em.persist(vote2);
- *             em.persist(pollUser2);
- *
- */
-
