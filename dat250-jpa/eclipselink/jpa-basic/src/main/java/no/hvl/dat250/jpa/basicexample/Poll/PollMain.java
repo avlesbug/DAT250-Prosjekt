130a171bb737df5c60b08493bb903e27eb8005ed
Kristian Avlesbug <kristian.avlebsug@student.uib.no>
2021-11-01 16:43:31 +0100

    votesforpoll viser nå riktig svaralternativ.

--- dat250-jpa/eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Poll/PollMain.java
+++ dat250-jpa/eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Poll/PollMain.java
@@ -202,9 +202,9 @@ public class PollMain {
             try {
                 Long id = Long.parseLong(req.params("id"));
                 Poll poll = pollDAO.findById(id);
-                int yVotes = poll.getOpt1Votes();
-                int nVotes = poll.getOpt2Votes();
-                return gson.toJson("Yes votes: " + yVotes + ", No votes: " + nVotes);
+                int opt1Votes = poll.getOpt1Votes();
+                int opt2Votes = poll.getOpt2Votes();
+                return gson.toJson(poll.getOpt1().toString() + " votes: " + opt1Votes + ", " + poll.getOpt2().toString() +" votes " + opt2Votes);
             }catch (Exception e) {
                 System.out.println(e.getStackTrace());
                 return gson.toJson("Something went wrong...");
